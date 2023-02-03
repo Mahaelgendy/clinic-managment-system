3ed183915f38b27a2b2e6b2a54b55de8c19045b0
Radwa Elgammal <elgammalr74@gmail.com>
2023-02-03 23:06:23 +0200

    some modifications on patient controller

--- Routes/patient.js
+++ Routes/patient.js
@@ -6,10 +6,10 @@ const router= express.Router();
 router.route("/patiets")
         .get(controller.getAllPatients)
         .post(controller.addPatient)
-        .patch(controller.updatePatient)
         .delete(controller.deletePatients);
 
 router.get("/patients/:id",controller.getPatientByID);
 router.delete("/patients/:id",controller.deletePatientById)
+router.patch("/patients/:id",controller.updatePatient)
 
 module.exports = router;
