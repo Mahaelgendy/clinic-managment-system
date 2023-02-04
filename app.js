45da2bcb4a3fb32873e49dda6427ddf3258c80cc
Radwa Elgammal <elgammalr74@gmail.com>
2023-02-04 14:57:01 +0200

    add patient routes

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const  morgan = require('morgan');
 const server=express();
 const appointmentRoutes = require("./Routes/appointment");
-
-
+const patientRoutes = require ("./Routes/patient")
 let port=process.env.PORT||8080;
 
 mongoose.set('strictQuery', true);
@@ -27,7 +26,7 @@ server.use(express.json());
 
 //routes
 server.use(appointmentRoutes);
-
+server.use(patientRoutes);
 //Not Found MW
 server.use((request ,response, next)=>{
     response.status(404).json({data:"Not Fount"});
