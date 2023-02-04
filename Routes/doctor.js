52d12623d47e95b17141c0a8cf01e63585c87011
Aya-Khaled562 <77298939+Aya-Khaled562@users.noreply.github.com>
2023-02-04 15:13:11 +0200

    mod

--- Routes/doctor.js
+++ Routes/doctor.js
@@ -3,19 +3,15 @@ const express = require('express');
 const controller = require('./../Controllers/doctor')
 const router = express.Router()
 
-router.route("/doctor/:id")
+router.route("/doctor/:id)
     .get(controller.getDoctorById)
     .delete(controller.deleteDoctor)
     .patch(controller.updateDoctor);
 
-<<<<<<< HEAD
-router.post('/doctor' , controller.addDoctor)
-=======
 
-router.route("/doctor")
+router.route('/doctor')
       .get(controller.getAllDoctors)
       .post(controller.addDoctor);
->>>>>>> 01b2643128c99c3c53ce64587fbca55b6320ac24
 
 module.exports = router
 
