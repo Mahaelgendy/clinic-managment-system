3ba52c3fb3658d2b1c80f54d7e281a7458220b0f
Aya-Khaled562 <77298939+Aya-Khaled562@users.noreply.github.com>
2023-02-05 16:28:53 +0200

    aya

--- Controllers/user.js
+++ Controllers/user.js
@@ -6,68 +6,6 @@ const { request, response } = require('express');
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
-exports.getAllUsers = (request , response , next)=>{
-    UserSchema.find()
-        .then(data=>{
-            if(data!=null){
-                response.status(200).json(data);
-            }
-        })
-        .catch(error=>next(error));
-
-}
-
-exports.getAllUsersByRole = (request , response , next)=>{
-    const role = request.params.role
-    UserSchema.find({role:role})
-    .then(data=>{
-        if(data!=null){
-            response.status(200).json(data);
-        }else{
-            response.status(200).json({message:"No Data for this role"})
-        }
-    })
-    .catch(error=>next(error));
-}
-
-exports.getUserById = (request , response , next)=>{
-    const id = request.params.id;
-    UserSchema.findOne({_id:id})
-    .then(data=>{
-        if(data!=null){
-            response.status(200).json(data);
-        }else{
-            response.status(200).json({message:"Id is not found"})
-        }
-    })
-    .catch(error=>next(error));
-}
-
-exports.getUserByEmail = (request , response , next)=>{
-    const email = request.params.email;
-    UserSchema.find({email:email})
-    .then(data=>{
-        if(data!=null){
-            response.status(200).json(data);
-        }else{
-            response.status(200).json({message:"Email is not found"})
-        }
-    })
-    .catch(error=>next(error));
-}
-
-exports.getUserByName = (request , response , next)=>{
-    const name = request.params.name;
-    UserSchema.find({fullName:name})
-    .then(data=>{
-        if(data!=null){
-            response.status(200).json(data);
-        }else{
-            response.status(200).json({message:"Name is not found"})
-        }
-    })
-    .catch(error=>next(error));
-}
 
 exports.addUser = (request, response , next)=>{
     const {fullName,password,email,age,gender,address,role} = request.body;
@@ -92,73 +30,57 @@ exports.addUser = (request, response , next)=>{
         .catch(err=>next(err));
 }
 
-exports.deleteUserById = (request , response , next)=>{
-    try{
-        const id = request.params.id;
-        UserSchema.findByIdAndDelete({_id:id})
-        .then(res=>{
-            response.status(200).json({message:"User deleted"});
-        })
-        .catch(err=>next(err));
+exports.getAllUsers = (request , response , next)=>{
+    const query = {};
+    if (request.query.fullName) query.fullName = request.query.fullName;
+    if (request.query.id) query._id = mongoose.Types.ObjectId(request.query.id);
+    if (request.query.role) query.role = request.query.role;
+    if (request.query.email) query.email = request.query.email;
 
-    }catch(error){
-        next(error)
+    UserSchema.find(query)
+    .then(data=>{
+        if(data!=null){
+            response.status(200).json(data);
+        }
+    })
+    .catch(error=>next(error));
+};
+
+exports.deleteUsers = (request , response , next)=>{
+    try{
+        const query = {};
+        if (request.query.fullName) query.fullName = request.query.fullName;
+        if (request.query.id) query._id = mongoose.Types.ObjectId(request.query.id);
+        if (request.query.role) query.role = request.query.role;
+        if (request.query.email) query.email = request.query.email;
+
+        UserSchema.deleteMany(query)
+        .then(data=>{
+            if(data!=null){
+                response.status(200).json({message:"User deleted"});
+            }
+        })
+        .catch(error=>next(error));
+
+    }catch(err){
+        next(err)
     }
     
-}
+};
 
-exports.deleteUserByEmail = (request,response, next)=>{
+exports.updateUser = (request,response,next)=>{
     try{
-        const email = request.params.email;
-        UserSchema.deleteOne({email:email})
-        .then(res=>{
-            response.status(200).json({message:"User deleted"});
-        })
-        .catch(err=>next(err));
+        const query = {};
+        if (request.query.fullName) query.fullName = request.query.fullName;
+        if (request.query.id) query._id = mongoose.Types.ObjectId(request.query.id);
+        if (request.query.email) query.email = request.query.email;
 
-    }catch(error){
-        next(error)
-    }
-}
-
-exports.updateUserByEmail = (request,response,next)=>{
-    try{
-        const emailreq = request.params.email;
         const {fullName,password,email,age,gender,address,role} = request.body;
 
         const salt = bcrypt.genSaltSync(saltRounds);
         const hash = bcrypt.hashSync(password, salt);
 
-        UserSchema.updateOne({email:emailreq},
-            {$set:{
-                fullName:fullName,
-                password:hash,
-                email:email,
-                age:age,
-                gender:gender,
-                address:address,
-                role:role
-            }})
-            .then(res=>{
-                response.status(200).json({message:"User Updated"})
-            })
-            .catch(err=>next(err));
-
-    }catch(error){
-        next(error)
-    }
-    
-}
-
-exports.updateUserById = (request,response,next)=>{
-    try{
-        const id = request.params.id;
-        const {fullName,password,email,age,gender,address,role} = request.body;
-
-        const salt = bcrypt.genSaltSync(saltRounds);
-        const hash = bcrypt.hashSync(password, salt);
-
-        UserSchema.updateOne({_id:id},
+        UserSchema.updateOne({query},
             {$set:{
                 fullName:fullName,
                 password:hash,
@@ -178,5 +100,69 @@ exports.updateUserById = (request,response,next)=>{
     }
     
 }
+
+
+
+// exports.updateUserByEmail = (request,response,next)=>{
+//     try{
+//         const emailreq = request.params.email;
+//         const {fullName,password,email,age,gender,address,role} = request.body;
+
+//         const salt = bcrypt.genSaltSync(saltRounds);
+//         const hash = bcrypt.hashSync(password, salt);
+
+//         UserSchema.updateOne({email:emailreq},
+//             {$set:{
+//                 fullName:fullName,
+//                 password:hash,
+//                 email:email,
+//                 age:age,
+//                 gender:gender,
+//                 address:address,
+//                 role:role
+//             }})
+//             .then(res=>{
+//                 response.status(200).json({message:"User Updated"})
+//             })
+//             .catch(err=>next(err));
+
+//     }catch(error){
+//         next(error)
+//     }
+    
+// }
+
+// exports.updateUserById = (request,response,next)=>{
+//     try{
+//         const id = request.params.id;
+//         const {fullName,password,email,age,gender,address,role} = request.body;
+
+//         const salt = bcrypt.genSaltSync(saltRounds);
+//         const hash = bcrypt.hashSync(password, salt);
+
+//         UserSchema.updateOne({_id:id},
+//             {$set:{
+//                 fullName:fullName,
+//                 password:hash,
+//                 email:email,
+//                 age:age,
+//                 gender:gender,
+//                 address:address,
+//                 role:role
+//             }})
+//             .then(res=>{
+//                 response.status(200).json({message:"User Updated"})
+//             })
+//             .catch(err=>next(err));
+            
+//     }catch(error){
+//         next(error)
+//     }
+    
+// }
+
+
+//-----------------------------------------------------------------//
+
 
 
--- Routes/user.js
+++ Routes/user.js
@@ -13,44 +13,26 @@ router.route("/users")
         userValidation.userbodyValidation,
         validator,
         controller.addUser)
-
-router.route("/users/role/:role")
-    .get(controller.getAllUsersByRole)
-
-router.route("/users/:id")
-    .get(
-        userValidation.useridValidaion,
-        validator,
-        controller.getUserById)
-    .delete(
-        userValidation.useridValidaion,
-        validator,
-        controller.deleteUserById)
+    .delete(controller.deleteUsers)
     .patch(
-        userValidation.useridValidaion,
         userValidation.userbodyValidation,
         validator,
-        controller.updateUserById)
+        controller.updateUser)
 
-router.route("/users/email/:email")
-    .get(
-        userValidation.userEmailValidation,
-        validator,
-        controller.getUserByEmail)
-    .delete(
-        userValidation.userEmailValidation,
-        validator,
-        controller.deleteUserByEmail)
-    .patch(
-        userValidation.userEmailValidation,
-        userValidation.userbodyValidation,
-        validator,
-        controller.updateUserByEmail)
 
-router.route("/users/name/:name")
-    .get(
-        userValidation.userNameValidation,
-        validator,
-        controller.getUserByName)
+// router.route("/users/:id")
+//     .patch(
+//         userValidation.useridValidaion,
+//         userValidation.userbodyValidation,
+//         validator,
+//         controller.updateUserById)
+
+// router.route("/users/email/:email")
+//     .patch(
+//         userValidation.userEmailValidation,
+//         userValidation.userbodyValidation,
+//         validator,
+//         controller.updateUserByEmail)
+
 
 module.exports = router
