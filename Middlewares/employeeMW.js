70964bda3de1418fec5de489396d5c7099565579
Radwa Elgammal <elgammalr74@gmail.com>
2023-02-07 01:35:21 +0200

    employeeMW and patient MW

--- Middlewares/employeeMW.js
+++ Middlewares/employeeMW.js
@@ -5,9 +5,7 @@ exports.employeevalidation = [
     body('employeeSalary').isInt().withMessage("salary should be integer"),
     body('employeePhone').isMobilePhone().matches(/^01[0-2,5]\d{8}$/).withMessage("phone number should start with 010- 012-015-011"),
     body('employeePosition').isString().isIn(['receptionist', 'assistant']).withMessage("Position shoud be string and 'receptionist' or 'assistant' "),
-    body('employeeID').isMongoId().withMessage("employee id should be mongo id"),
-    body('clinic_Id').isInt().withMessage("clinic id should be integer"),
-
+    body('clinic_Id').isInt().withMessage("clinic id should be integer")
 ]
 
 exports.paramvalidation = [
--- Middlewares/patientMW.js
+++ Middlewares/patientMW.js
@@ -7,8 +7,7 @@ exports.patientvalidation = [
     body('patientHeight').isInt().withMessage("height shouls be integer"),
     body('patientWeight').isInt().withMessage("weight should be integer"),
     body('patientHasInsurance').isBoolean().withMessage("hasInsurance should be true or false"),
-    body('patientPhone').isMobilePhone().matches(/^01[0-2,5]\d{8}$/).withMessage("phone number should start with 010- 012-015-011"),
-    body('patientID').isMongoId().withMessage("patientID should be mongo id")
+    body('patientPhone').isMobilePhone().matches(/^01[0-2,5]\d{8}$/).withMessage("phone number should start with 010- 012-015-011")
 ]
 
 exports.paramvalidation = [
