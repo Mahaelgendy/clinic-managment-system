cd23d3c077071012f85ebd0393dcf85a1d52b0c6
zienab <zienab.hesham1999@gmail.com>
2023-04-04 23:59:35 +0200

    clinic,service,schedule

--- Middlewares/scheduleMW.js
+++ Middlewares/scheduleMW.js
@@ -1,12 +1,12 @@
 const {body, param} = require("express-validator");
 
 exports.bodyValidation = [
-    body("doctor_id").isInt().withMessage("Doctor ID must be Numeric and required"),
+    body("doc_id").isInt().withMessage("Doctor ID must be Numeric and required"),
     body("clinic_id").isInt().withMessage("ClinicID must be Numeric and required"),
     body("date").isString().notEmpty().matches(/^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/).withMessage("Date must be string in format yyyy-mm-dd"),
     body("from").isString().notEmpty().matches(/^(?:(?:[0-1][0-9]|2[0-3]):[0-5]?[0-9](?::[0-5]?[0-9])?)|(?:[0-9]:[0-5]?[0-9](?::[0-5]?[0-9])?)$/).withMessage("Start time must be string in format hh:mm:ss"),
     body("to").isString().notEmpty().matches(/^(?:(?:[0-1][0-9]|2[0-3]):[0-5]?[0-9](?::[0-5]?[0-9])?)|(?:[0-9]:[0-5]?[0-9](?::[0-5]?[0-9])?)$/).withMessage("End time must be string in format hh:mm:ss"),
-    body('duration').isInt().withMessage("Duration must be number ")
+    body('duration_in_minutes').isInt().withMessage("Duration must be number ")
 ]
 
 exports.paramValidation = [
